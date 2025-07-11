4590af003957855d9a3f36acf5424e52bd27e12d
Jonathan Sack <dev@jonathansack.io>
2025-07-11 21:03:36 +0200

    Removed test target

--- Package.swift
+++ Package.swift
@@ -22,8 +22,5 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "JSKeychain"),
-        .testTarget(
-            name: "JSKeychainTests",
-            dependencies: ["JSKeychain"]),
     ]
 )
